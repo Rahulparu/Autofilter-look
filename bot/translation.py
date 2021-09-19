7ec2fafc9fce575299c29326201d704a9b3f8a8f
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2021-09-19 09:09:06 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -56,5 +56,5 @@ You are able to contact me:--> @RahulrsParu 📵
 
 <b>➥ My Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/Anandhu123555/Autofilter-look">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a 😠😠 </a></i>
 """
