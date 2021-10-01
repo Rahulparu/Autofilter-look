02e86e2ace1f66a9a1eb598b293b128eb755cac3
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2021-10-01 11:57:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -120,7 +120,7 @@ async def start(bot, update):
     await bot.send_photo(
         chat_id=update.chat.id,
         chat_id=update.chat.id,
-        photo="https://telegra.ph/file/c2edac4a6cd119b61e9a9.jpg",
+        photo="https://telegra.ph/file/a9b419a8786a4a1608947.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
